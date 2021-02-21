a0a3fd221bb869dc655e11c6306475abf4915243
Ali Qasemi <qasemi4245qq@gmail.com>
2021-02-21 11:20:50 +0330

    .

--- app/Helpers/HasComment.php
+++ app/Helpers/HasComment.php
@@ -4,13 +4,14 @@
 namespace App\Helpers;
 
 
+use App\Models\Comment;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait HasComment {
 
     public function comments() : MorphMany
     {
-        return $this->morphMany(...config('common.morph_models.comment'));
+        return $this->morphMany(Comment::class, 'commentable');
     }
 
 }
