60ca5f459f9e7aa51a2941473ace220a9ea46f24
Ali Qasemi <qasemi4245qq@gmail.com>
2021-02-21 10:48:40 +0330

    set relation on user and transplantation

--- app/Models/Transplantation.php
+++ app/Models/Transplantation.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transplantation extends Model
 {
@@ -15,11 +16,15 @@ class Transplantation extends Model
         return 'transplantation';
     }
 
-    protected $fillable = ['name', 'category'];
+    protected $fillable = ['name', 'category', 'user_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function portfolio(): HasMany{
         return $this->hasMany(Portfolio::class);
     }
-
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -50,4 +51,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function transplantations(): HasMany
+    {
+        return $this->hasMany(Transplantation::class);
+    }
 }
