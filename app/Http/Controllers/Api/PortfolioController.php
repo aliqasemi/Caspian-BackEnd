ee4e23ad05f688a154421649f67e81877ce595f1
Ali Qasemi <qasemi4245qq@gmail.com>
2021-02-27 09:58:56 +0330

    store portfolio and set valid transplantation id on portfolio request

--- app/Http/Controllers/Api/PortfolioController.php
+++ app/Http/Controllers/Api/PortfolioController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Portfolio\PortfolioStoreRequest;
 use App\Http\Requests\Portfolio\PortfolioUpdateRequest;
+use App\Http\Resources\PortfolioResource;
 use App\Models\Portfolio;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 
 class PortfolioController extends Controller
 {
@@ -23,19 +26,34 @@ class PortfolioController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return PortfolioResource
      */
     public function store(PortfolioStoreRequest $request)
     {
         $this->authorize('create', Portfolio::class);
 
+        $data = $request->all();
+
+        $portfolio = Portfolio::create([
+            'title' => Arr::get($data, 'title'),
+            'description' => Arr::get($data, 'description'),
+            'transplantation_id' => Arr::get($data, 'transplantation_id')
+        ]);
+
+        $portfolio->save();
+
+        $portfolio->load(['transplantation', 'transplantation.user']);
+
+        return new PortfolioResource(
+            $portfolio
+        );
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Portfolio  $portfolio
+     * @param \App\Models\Portfolio $portfolio
      * @return \Illuminate\Http\Response
      */
     public function show(Portfolio $portfolio)
@@ -47,8 +65,8 @@ class PortfolioController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Portfolio  $portfolio
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Portfolio $portfolio
      * @return \Illuminate\Http\Response
      */
     public function update(PortfolioUpdateRequest $request, Portfolio $portfolio)
@@ -60,7 +78,7 @@ class PortfolioController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Portfolio  $portfolio
+     * @param \App\Models\Portfolio $portfolio
      * @return \Illuminate\Http\Response
      */
     public function destroy(Portfolio $portfolio)
