3b4ce2971e4d75f33bd7df55415b8316c7207931
Ali Qasemi <qasemi4245qq@gmail.com>
2021-02-27 23:35:44 +0330

    create Education resource

---
+++ app/Http/Resources/EducationResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EducationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->description,
+            'portfolios' => new PortfolioResource($this->whenLoaded('portfolio'))
+        ];
+    }
+}
