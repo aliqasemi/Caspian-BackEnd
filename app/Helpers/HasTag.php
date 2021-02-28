9d93f581427e12227c48d46420bea07503bad542
Ali Qasemi <qasemi4245qq@gmail.com>
2021-02-28 12:42:47 +0330

    complete Tag durable

--- app/Helpers/HasTag.php
+++ app/Helpers/HasTag.php
@@ -1,14 +1,58 @@
 <?php
 
-
 namespace App\Helpers;
 
 use App\Models\tag;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 trait HasTag
 {
+    private $new_tags = [];
+    private $sync_tag = [];
+
+    /**
+     * @param mixed $tagRequest
+     */
+    public function setTagSync($tagRequest): void
+    {
+        foreach ($tagRequest as $tag) {
+            if (!is_numeric($tag)) {
+                $newTag = tag::create([
+                    'name' => $tag,
+                    'user_id' => Auth::id()
+                ]);
+                $this->new_tags[] = $newTag;
+                $this->sync_tag[] = $newTag->id;
+            } else {
+                $this->sync_tag[] = $tag;
+            }
+        }
+
+        $this->sync();
+    }
+
     public function tags()
     {
         return $this->morphToMany(Tag::class, 'taggable');
+    }
+
+    public function syncTag()
+    {
+        $this->validate(request()->get('tags'));
+        $this->setTagSync(request()->get('tags'));
+    }
+
+    public function sync(){
+        $this->tags()->saveMany($this->new_tags);
+        $this->tags()->sync($this->sync_tag);
+    }
+
+    private function validate($tags)
+    {
+        Validator::make($tags, [
+            'tags' => 'nullable|array',
+            'tags.*' => 'required|string|distinct',
+        ])->validate();
     }
 }
