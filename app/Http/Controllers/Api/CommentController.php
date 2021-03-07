9a9f99509ec7016e9ed1cd919689cfd1d1889092
Ali Qasemi <qasemi4245qq@gmail.com>
2021-03-07 13:19:12 +0330

    delete comment

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -108,10 +108,13 @@ class CommentController extends Controller
      * Remove the specified resource from storage.
      *
      * @param \App\Models\Comment $comment
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Comment $comment)
     {
-        //
+        $response = $comment->delete();
+
+        if ($response == true)
+            return response()->json('عملیات با موفقیت انجام شد');
     }
 }
