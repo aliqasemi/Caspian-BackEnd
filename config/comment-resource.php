5ae7cf7c165451d8c7e83b16aec6ea0f69677251
Ali Qasemi <qasemi4245qq@gmail.com>
2021-03-07 13:09:52 +0330

    create index request

---
+++ config/comment-resource.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'resource' => [
+        'education' => \App\Http\Resources\EducationResource::class,
+        'portfolio' => \App\Http\Resources\PortfolioResource::class,
+        'transplantation' => \App\Http\Resources\TransplantationResource::class,
+    ],
+];
