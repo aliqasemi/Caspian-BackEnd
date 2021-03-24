d8b2fe40b513dc446fc0a1274eab51b217c5d982
Ali Qasemi <qasemi4245qq@gmail.com>
2021-03-24 10:37:19 +0430

    create config file for import requred models on laravel scoute command

--- config/models.php
+++ config/models.php
@@ -1,1 +1,10 @@
 <?php
+
+return [
+    'models' => [
+        'user' => App\Models\User::class,
+        'education' => \App\Models\Education::class,
+        'portfolio' => \App\Models\Portfolio::class,
+        'transplantation' => \App\Models\Transplantation::class,
+    ],
+];
